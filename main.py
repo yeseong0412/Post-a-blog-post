b78a7daf4e1c5d82e28eaf7a35d8dbe0f8d89dd1
양예성 <127856153+yeseong0412@users.noreply.github.com>
2023-05-20 01:27:01 +0900

    예외 처리 부분 보강 및 추가
    여러 상황을 예외처리에 추가시킴

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 def post_to_tistory(title, content, tags):
     # 티스토리 API 엔드포인트와 인증 정보 설정
     api_url = 'https://www.tistory.com/apis/post/write'
-    access_token = 'YOUR_TISTORY_ACCESS_TOKEN'
-    blog_name = 'YOUR_TISTORY_BLOG_NAME'
+    access_token = 'YOUR_TISTORY_ACCESS_TOKEN'  # 티스토리 액세스 토큰을 여기에 입력하세요
+    blog_name = 'YOUR_TISTORY_BLOG_NAME'  # 티스토리 블로그 이름을 여기에 입력하세요
 
     # API 요청 데이터 설정
     data = {
@@ -20,19 +20,30 @@ def post_to_tistory(title, content, tags):
     try:
         # API 요청 보내기
         response = requests.post(api_url, data=data)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
+        response.raise_for_status()
 
-        if response.status_code == 200:
+        if response.status_code == 200: #200은 정상작동 됬다는 뜻
             print('티스토리에 글이 성공적으로 작성되었습니다.')
         else:
             print('티스토리에 글 작성 실패:', response.text)
     except requests.exceptions.RequestException as e:
         print('티스토리 API 요청 에러:', e)
+    except requests.exceptions.HTTPError as e:
+        print('HTTP 에러 발생:', e)
+    except requests.exceptions.ConnectionError as e:
+        print('네트워크 연결 오류:', e)
+    except requests.exceptions.Timeout as e:
+        print('요청 시간 초과:', e)
+    except requests.exceptions.TooManyRedirects as e:
+        print('너무 많은 리다이렉션 발생:', e)
+    except Exception as e:
+        print('예외가 발생했습니다:', e)
+
 
 def post_to_blogger(title, content, labels):
     # Blogger API 엔드포인트와 인증 정보 설정
     api_url = 'https://www.googleapis.com/blogger/v3/blogs/BLOG_ID/posts'
-    api_key = 'YOUR_BLOGGER_API_KEY'
+    api_key = 'YOUR_BLOGGER_API_KEY'  # Blogger API 키를 여기에 입력하세요
 
     # API 요청 헤더 설정
     headers = {
@@ -43,16 +54,17 @@ def post_to_blogger(title, content, labels):
     data = {
         'kind': 'blogger#post',
         'blog': {
-            'id': 'BLOG_ID'
+            'id': 'BLOG_ID'  # 블로그 ID를 여기에 입력하세요
         },
         'title': title,
         'content': content,
         'labels': labels
     }
+
     try:
         # API 요청 보내기
         response = requests.post(api_url, headers=headers, json=data, params={'key': api_key})
-        response.raise_for_status()  # Raise an exception for non-200 status codes
+        response.raise_for_status()
 
         if response.status_code == 200:
             print('Blogger에 글이 성공적으로 작성되었습니다.')
@@ -60,8 +72,16 @@ def post_to_blogger(title, content, labels):
             print('Blogger에 글 작성 실패:', response.text)
     except requests.exceptions.RequestException as e:
         print('Blogger API 요청 에러:', e)
-
-
+    except requests.exceptions.HTTPError as e:
+        print('HTTP 에러 발생:', e)
+    except requests.exceptions.ConnectionError as e:
+        print('네트워크 연결 오류:', e)
+    except requests.exceptions.Timeout as e:
+        print('요청 시간 초과:', e)
+    except requests.exceptions.TooManyRedirects as e:
+        print('너무 많은 리다이렉션 발생:', e)
+    except Exception as e:
+        print('예외가 발생했습니다:', e)
 
 
 # 티스토리에 글 작성
