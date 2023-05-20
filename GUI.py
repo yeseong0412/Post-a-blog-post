7a391e97c1c75102400c9be8ad29ab11c3bd698e
양예성 <127856153+yeseong0412@users.noreply.github.com>
2023-05-21 02:02:44 +0900

    한글 출력 이슈 수정

--- GUI.py
+++ GUI.py
@@ -7,19 +7,19 @@ def submit_post():
     content = content_text.get("1.0", tk.END)
     post_to_tistory(title, content)
 window = tk.Tk()
-window.title("티스토리 글 작성")
+window.title("blog write") #티스토리 글 작성 -> blog write (한글이 깨져서 출력됨)
 
-title_label = tk.Label(window, text="제목")
+title_label = tk.Label(window, text="title")
 title_label.pack()
 title_entry = tk.Entry(window)
 title_entry.pack()
 
-content_label = tk.Label(window, text="내용")
+content_label = tk.Label(window, text="content")
 content_label.pack()
 content_text = tk.Text(window)
 content_text.pack()
 
-submit_button = tk.Button(window, text="글 작성", command=submit_post)
+submit_button = tk.Button(window, text="write", command=submit_post)
 submit_button.pack()
 
 window.mainloop()
